7d07444e73076d0c5cf280528143349df3a0b708
HowardCheng <makubex49@gmail.com>
2020-12-04 15:10:00 +0800

    增加存取大量資料的方法

--- My_INI_db/INI_db.cs
+++ My_INI_db/INI_db.cs
@@ -12,15 +12,15 @@ namespace AIM.Modules
 {
     class INI_db
     {
-        public void Save(string db, string group, string parameter, string value)
+        public void Save(string db_path, string group, string parameter, string value)
         {
             var parser = new FileIniDataParser();
-            if (File.Exists(db))
+            if (File.Exists(db_path))
             {
                 IniData data;
                 try
                 {
-                    data = parser.ReadFile(db);
+                    data = parser.ReadFile(db_path);
                 }
                 catch (Exception ex)
                 {
@@ -32,12 +32,12 @@ namespace AIM.Modules
 
                 }
                 data[group][parameter] = value;
-                parser.WriteFile(db, data);
+                parser.WriteFile(db_path, data);
             }
             else
             {
 
-                string directory = Path.GetDirectoryName(db);
+                string directory = Path.GetDirectoryName(db_path);
 
                 if (directory != "" && !Directory.Exists(directory))
                 {
@@ -46,24 +46,25 @@ namespace AIM.Modules
 
                 IniData data = new IniData();
                 data[group][parameter] = value;
-                parser.WriteFile(db, data);
+                parser.WriteFile(db_path, data);
             }
         }
         
-        public void Save<T>(string db, string group, string parameter, T value)
+        public void Save<T>(string db_path, string group, string parameter, T value)
         {
-            this.Save(db, group, parameter, Convert.ToString(value));
+            this.Save(db_path, group, parameter, Convert.ToString(value));
         }
 
-        public string Load(string db, string group, string parameter)
+
+        public string Load(string db_path, string group, string parameter)
         {
             IniData data;
-            if (File.Exists(db))
+            if (File.Exists(db_path))
             {
                 try
                 {
                     var parser = new FileIniDataParser();
-                    data = parser.ReadFile(db);
+                    data = parser.ReadFile(db_path);
                 }
                 catch (Exception ex)
                 {
@@ -79,18 +80,18 @@ namespace AIM.Modules
             }
 
             return data[group][parameter];
-        }
+        }        
                 
-        public T Load<T>(string db, string group, string parameter, T default_value)
+        public T Load<T>(string db_path, string group, string parameter, T default_value)
         {
             T value;
-            return this.TryLoad(db, group, parameter, out value) ? value : default_value;
+            return this.TryLoad(db_path, group, parameter, out value) ? value : default_value;
         }        
 
-        public bool TryLoad<T>(string db, string group, string parameter, out T value)
+        public bool TryLoad<T>(string db_path, string group, string parameter, out T value)
         {
             // https://stackoverflow.com/questions/2961656/generic-tryparse
-            string value_str = this.Load(db, group, parameter);
+            string value_str = this.Load(db_path, group, parameter);
             try
             {
                 var converter = TypeDescriptor.GetConverter(typeof(T));
@@ -110,5 +111,99 @@ namespace AIM.Modules
             }
         }
 
+        public IniData Load(string db_path)
+        {
+            IniData data;
+            if (File.Exists(db_path))
+            {
+                try
+                {
+                    var parser = new FileIniDataParser();
+                    data = parser.ReadFile(db_path);
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(
+                        $">>{ex.Message}\r\n" +
+                        $"{ex.ToString()}");
+                    data = new IniData();
+                }
+            }
+            else
+            {
+                data = new IniData();
+            }
+
+            return data;
+        }
+        
+        public bool TryLoad<T>(IniData db_mem, string group, string parameter, out T value)
+        {
+            string value_str = db_mem[group][parameter];
+            try
+            {
+                var converter = TypeDescriptor.GetConverter(typeof(T));
+                if (converter != null)
+                {
+                    // Cast ConvertFromString(string text) : object to (T)
+                    value = (T)converter.ConvertFromString(value_str);
+                    return true;
+                }
+                value = default(T);
+                return false;
+            }
+            catch (Exception)
+            {
+                value = default(T);
+                return false;
+            }
+        }
+
+        public T Load<T>(IniData db_mem, string group, string parameter, T default_value)
+        {
+            T value;
+            return this.TryLoad(db_mem, group, parameter, out value) ? value : default_value;
+        }
+
+        public void Insert<T>(IniData db_mem, string group, string parameter, T value)
+        {
+            db_mem[group][parameter] = Convert.ToString(value);
+        }
+
+        public void Save(string db_path, IniData db_mem)
+        {
+            var parser = new FileIniDataParser();
+            if (File.Exists(db_path))
+            {
+                IniData data;
+                try
+                {
+                    data = parser.ReadFile(db_path);
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(
+                        $">>{ex.Message}\r\n" +
+                        $"{ex.ToString()}");
+
+                    data = new IniData();
+                }
+                data.Merge(db_mem);
+                parser.WriteFile(db_path, data);
+            }
+            else
+            {
+
+                string directory = Path.GetDirectoryName(db_path);
+
+                if (directory != "" && !Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                IniData data = new IniData();
+                parser.WriteFile(db_path, db_mem);
+            }
+        }
     }
 }
--- readme.md
+++ readme.md
@@ -14,15 +14,15 @@ Then you can import `INI_db.cs` with `Join existing item` .
 
 ### Save parameters
 
-```cs
-// db
-INI_db db = new INI_db();           
+```c#
+INI_db db_tool = new INI_db();           
 string db_path = "db.ini";
 
-db.Save(db_path, "Group1", "Boolean", true);
-db.Save(db_path, "Group1", "Integer", 100);
-db.Save(db_path, "Group1", "Float", 123.321);
-db.Save(db_path, "Group1", "String", "Hello");
+// save example
+db_tool.Save(db_path, "Group1", "Boolean", true);
+db_tool.Save(db_path, "Group1", "Integer", 100);
+db_tool.Save(db_path, "Group1", "Float", 123.321);
+db_tool.Save(db_path, "Group1", "String", "Hello");
 ```
 
 And you can get files`db.ini` like below
@@ -40,12 +40,12 @@ String = Hello
 By this method a preset value can be specified when reading parameters. Avoid parameter does not exist.
 
 ```csharp
-// db
 INI_db db = new INI_db();
 string db_path = "db.ini";
 
-System.Console.WriteLine("data = {0}", db.Load(db_path, "Group1", "Integer", value_default));
-System.Console.WriteLine("data = {0}", db.Load(db_path, "Group1", "Integer_1", value_default));
+int value_default = 123;
+System.Console.WriteLine("data = {0}", db_tool.Load(db_path, "Group1", "Integer", value_default));
+System.Console.WriteLine("data = {0}", db_tool.Load(db_path, "Group1", "Integer_1", value_default));
 
 //data = 100
 //data = 123
@@ -55,13 +55,46 @@ Or try to load like this method
 
 ```c#
 double oValue;
-if (db.TryLoad(db_path, "Group1", "Float", out oValue))
+if (db_tool.TryLoad(db_path, "Group1", "Float", out oValue))
     Console.WriteLine($"Load Success, data = {oValue}");
 else
     Console.WriteLine("Load Failed");
 ```
 
+### The amount of data is a little big?
 
+It takes a lot of time because of repeated reading and writing file... But you can modify data before save the db file `data.ini` by below method.
 
+```c#
+// Save DB
+Random rand = new Random();
 
+int k_group_num = 50; // 50x100
+int k_para_num = 100;
+var db_mem = new IniData();
+for (int idx_group = 0; idx_group < k_group_num; idx_group++)
+{
+    for (int idx_para = 0; idx_para < k_para_num; idx_para++)
+    {
+        db_tool.Insert(db_mem, $"Group_{idx_group}", $"Value_{idx_para}", rand.NextDouble());
+    }
+}
+db_tool.Save("data.ini", db_mem);
+```
+
+Also you can get data after load the db file `data.ini` by below method.
+
+```c#
+// Load DB
+var db_mem = db_tool.Load("data.ini");
+for (int idx_group = 0; idx_group < k_group_num; idx_group++)
+{
+    for (int idx_para = 0; idx_para < k_para_num; idx_para++)
+    {
+        double f_value;
+        //db.TryLoad(db_mem, $"Group_{idx_group}", $"Value_{idx_para}", out f_value);
+        f_value = db_tool.Load(db_mem, $"Group_{idx_group}", $"Value_{idx_para}", 0.0);
+    }
+}
+```
 
