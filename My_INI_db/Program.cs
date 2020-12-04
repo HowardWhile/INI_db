3955d301c25b3c7e6ae2fd513608261b7e618774
HowardCheng <makubex49@gmail.com>
2020-12-04 09:55:41 +0800

    add load example

--- My_INI_db/Program.cs
+++ My_INI_db/Program.cs
@@ -11,22 +11,20 @@ namespace My_INI_db
     {
         static void Main(string[] args)
         {
-            INI_db db = new INI_db();
-
             // db
+            INI_db db = new INI_db();           
             string db_path = "db.ini";
 
-            // data
-            bool bool_value = true;
-            int int_value = 100;
-            double f_value = 123.321;
-            string str_value = "Hello";
+            db.Save(db_path, "Group1", "Boolean", true);
+            db.Save(db_path, "Group1", "Integer", 100);
+            db.Save(db_path, "Group1", "Float", 123.321);
+            db.Save(db_path, "Group1", "String", "Hello");
 
-            db.Save(db_path, "Group1", "Boolean", bool_value);
-            db.Save(db_path, "Group1", "Integer", int_value);
-            db.Save(db_path, "Group1", "Float", f_value);
-            db.Save(db_path, "Group1", "String", str_value);
-            
+            int value_default = 123;
+
+            System.Console.WriteLine("data = {0}", db.Load(db_path, "Group1", "Integer", value_default));
+            System.Console.WriteLine("data = {0}", db.Load(db_path, "Group1", "Integer_1", value_default));
+
 
             Console.ReadKey();
         }
